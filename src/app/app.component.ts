a8b93b1f34e250bbd2e4c7e5d6457772512f3af7
NataliaMelissa <u201816996@upc.edu.pe>
2026-05-05 23:06:52 -0500

    fix config and baseURL observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Colaborador } from './interfaces/colaborador';
+import { DivisionService } from './services/division.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'frontend-app';
+
+  //Variables - Listas
+  colaboradores: Colaborador[] = [];
+
+  //Consturctores
+  constructor(private divisionService: DivisionService) {}
+
+  //Función:
+  ngOnInit(): void {
+    //Llamar al servicio Colaboradores
+    this.divisionService.getColaboradores().subscribe({
+      next: (data) => {
+        this.colaboradores = data; // Guardamos los datos que llegaron
+        console.log('Datos recibidos:', data);
+      },
+      error: (e) => console.error('Error al llamar a Render:', e)
+    });
+  }
 }
+
--- src/app/services/division.service.ts
+++ src/app/services/division.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Colaborador } from '../interfaces/colaborador';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DivisionService {
 
-  constructor() { }
+  private baseUrl = 'https://divisiones-backend.onrender.com'; //URL Base del Backend
+
+  constructor(private http: HttpClient) { }
+
+  //Observables
+  getDivisiones(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}/division`);
+  }
+
+  getColaboradores(): Observable<Colaborador[]> {
+    return this.http.get<Colaborador[]>(`${this.baseUrl}/colaborador`);
+  }
 }
